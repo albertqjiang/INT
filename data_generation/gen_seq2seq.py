2b422112a328d2c2bc6db9c81d504bcf8e821ac1
Albert Jiang <albert594250@gmail.com>
2020-09-18 21:01:51 +0100

    adding degree and num of combos or orders generation mode

--- data_generation/gen_seq2seq.py
+++ data_generation/gen_seq2seq.py
@@ -43,18 +43,28 @@ if __name__ == "__main__":
     parser.add_argument('-l', type=int)
     parser.add_argument('--degree', type=int, default=0)
     parser.add_argument('--num_probs', type=int, default=1)
+    parser.add_argument('--use_combos', action='store_true')
+    parser.add_argument('--num_order_or_combo', type=int, default=None)
     args = parser.parse_args()
 
     if not os.path.isdir(args.dump_path):
         os.makedirs(args.dump_path)
 
+    combos = json.load(open(os.path.join(args.orders_path, "combos.json"), "r"))
     orders = json.load(open(os.path.join(args.orders_path, "orders.json"), "r"))
     if args.num_probs > 10000:
         sources_to_targets = None
         for j in range(int(args.num_probs/1000)):
-            datasets, problems = generate_multiple_problems(num_axioms=args.k, length=args.l,
-                                                            num_probs=1000, train_test="train",
-                                                            orders=orders, degree=args.degree)
+            if args.use_combos:
+                datasets, problems = generate_multiple_problems(num_axioms=args.k, length=args.l,
+                                                                num_probs=1000, train_test="train",
+                                                                combos=combos, degree=args.degree,
+                                                                num_order_or_combo=args.num_order_or_combo)
+            else:
+                datasets, problems = generate_multiple_problems(num_axioms=args.k, length=args.l,
+                                                                num_probs=1000, train_test="train",
+                                                                orders=orders, degree=args.degree,
+                                                                num_order_or_combo=args.num_order_or_combo)
             sources_to_targets = generate_multiple_seq2seq(multiple_problems=problems,
                                                            all_sources_to_targets=sources_to_targets)
             pickle.dump(problems, open(os.path.join(args.dump_path, "problems_part{}.pkl".format(j+1)), "wb"))
