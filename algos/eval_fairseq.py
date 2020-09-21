625c12765cd00397b2180d3b33be189e419475ca
Albert Jiang <albert594250@gmail.com>
2020-09-21 10:36:23 +0100

    improvement on evaluation

--- algos/eval_fairseq.py
+++ algos/eval_fairseq.py
@@ -9,6 +9,7 @@ Translate raw text with a trained model. Batches data on-the-fly.
 """
 
 from collections import namedtuple
+from copy import deepcopy
 import fileinput
 import logging
 import math
@@ -211,119 +212,124 @@ def main(args):
     test_problems = pickle.load(open(os.path.join(args.test_problems_path, "test_problems.pkl"), "rb"))
     proofs_closed = 0
     for test_problem in test_problems:
-        test_step_1 = test_problem[1]
-        test_prover = Prover(axioms=all_axioms_to_prove,
-                             conditions=test_step_1["observation"]["ground_truth"],
-                             objectives=test_step_1["observation"]["objectives"],
-                             prove_direction="backward")
-        for _ in range(15):
+        for _ in range(3):
+            # Every theorem gets three goes
+            test_step_1 = test_problem[1]
+            test_prover = Prover(axioms=all_axioms_to_prove,
+                                 conditions=test_step_1["observation"]["ground_truth"],
+                                 objectives=test_step_1["observation"]["objectives"],
+                                 prove_direction="backward")
+            for _ in range(15):
+                if test_prover.is_proved():
+                    break
+                source = test_prover.parser.observation_to_source(test_prover.get_observation())
+                inputs = [" ".join(filter_arrow(source))]
+
+                ###################################################
+                # INT stuff stops here
+                ###################################################
+
+                # for inputs in buffered_read(args.input, args.buffer_size):
+                results = []
+                for batch in make_batches(inputs, args, task, max_positions, encode_fn):
+                    bsz = batch.src_tokens.size(0)
+                    src_tokens = batch.src_tokens
+                    src_lengths = batch.src_lengths
+                    constraints = batch.constraints
+                    if use_cuda:
+                        src_tokens = src_tokens.cuda()
+                        src_lengths = src_lengths.cuda()
+                        if constraints is not None:
+                            constraints = constraints.cuda()
+
+                    sample = {
+                        'net_input': {
+                            'src_tokens': src_tokens,
+                            'src_lengths': src_lengths,
+                        },
+                    }
+                    translate_start_time = time.time()
+                    translations = task.inference_step(generator, models, sample, constraints=constraints)
+                    translate_time = time.time() - translate_start_time
+                    total_translate_time += translate_time
+                    list_constraints = [[] for _ in range(bsz)]
+                    if args.constraints:
+                        list_constraints = [unpack_constraints(c) for c in constraints]
+                    for i, (id, hypos) in enumerate(zip(batch.ids.tolist(), translations)):
+                        src_tokens_i = utils.strip_pad(src_tokens[i], tgt_dict.pad())
+                        constraints = list_constraints[i]
+                        results.append((start_id + id, src_tokens_i, hypos,
+                                        { "constraints": constraints,
+                                          "time": translate_time / len(translations) }
+                                    ))
+
+                # sort output to match input order
+                for id_, src_tokens, hypos, info in sorted(results, key=lambda x: x[0]):
+                    if src_dict is not None:
+                        src_str = src_dict.string(src_tokens, args.remove_bpe)
+                        f_out.write('S-{}\t{}\n'.format(id_, src_str))
+                        f_out.write("W-{}\t{:.3f}\tseconds\n".format(id_, info["time"]))
+                        for constraint in info["constraints"]:
+                            f_out.write("C-{}\t{}\n".format(id_, tgt_dict.string(constraint, args.remove_bpe)))
+
+                    # Process top predictions
+                    for hypo in hypos[:min(len(hypos), args.nbest)]:
+                        hypo_tokens, hypo_str, alignment = utils.post_process_prediction(
+                            hypo_tokens=hypo['tokens'].int().cpu(),
+                            src_str=src_str,
+                            alignment=hypo['alignment'],
+                            align_dict=align_dict,
+                            tgt_dict=tgt_dict,
+                            remove_bpe=args.remove_bpe,
+                            extra_symbols_to_ignore=get_symbols_to_strip_from_output(generator),
+                        )
+                        detok_hypo_str = decode_fn(hypo_str)
+                        score = hypo['score'] / math.log(2)  # convert to base 2
+                        # original hypothesis (after tokenization and BPE)
+                        f_out.write('H-{}\t{}\t{}\n'.format(id_, score, hypo_str))
+
+                        exec_string = chared_to_string(hypo_str.split())
+                        f_out.write("Execution string:" + exec_string + "\n")
+                        try:
+                            result = test_prover.apply_theorem_seq_style(exec_string)
+                            # f_out.write(result)
+                        except Exception:
+                            continue
+                        if test_prover.is_proved():
+                            proofs_closed += 1
+                            break
+                        f_out.write("Objec: " + " ".join(filter_arrow(
+                            test_prover.parser.observation_to_source(test_prover.get_observation()))) + "\n")
+
+                        if result["progress"]:
+                            break
+
+                        # detokenized hypothesis
+                        f_out.write('D-{}\t{}\t{}\n'.format(id_, score, detok_hypo_str))
+                        f_out.write('P-{}\t{}\n'.format(
+                            id_,
+                            ' '.join(map(
+                                lambda x: '{:.4f}'.format(x),
+                                # convert from base e to base 2
+                                hypo['positional_scores'].div_(math.log(2)).tolist(),
+                            ))
+                        ))
+                        if args.print_alignment:
+                            alignment_str = " ".join(["{}-{}".format(src, tgt) for src, tgt in alignment])
+                            f_out.write('A-{}\t{}\n'.format(
+                                id_,
+                                alignment_str
+                            ))
+
+                # update running id_ counter
+                start_id += len(inputs)
+            if test_prover.is_proved():
+                f_out.write("Proof closed\n")
+            if not test_prover.is_proved():
+                f_out.write("Proof failed\n")
+
             if test_prover.is_proved():
                 break
-            source = test_prover.parser.observation_to_source(test_prover.get_observation())
-            inputs = [" ".join(filter_arrow(source))]
-
-            ###################################################
-            # INT stuff stops here
-            ###################################################
-
-            # for inputs in buffered_read(args.input, args.buffer_size):
-            results = []
-            for batch in make_batches(inputs, args, task, max_positions, encode_fn):
-                bsz = batch.src_tokens.size(0)
-                src_tokens = batch.src_tokens
-                src_lengths = batch.src_lengths
-                constraints = batch.constraints
-                if use_cuda:
-                    src_tokens = src_tokens.cuda()
-                    src_lengths = src_lengths.cuda()
-                    if constraints is not None:
-                        constraints = constraints.cuda()
-
-                sample = {
-                    'net_input': {
-                        'src_tokens': src_tokens,
-                        'src_lengths': src_lengths,
-                    },
-                }
-                translate_start_time = time.time()
-                translations = task.inference_step(generator, models, sample, constraints=constraints)
-                translate_time = time.time() - translate_start_time
-                total_translate_time += translate_time
-                list_constraints = [[] for _ in range(bsz)]
-                if args.constraints:
-                    list_constraints = [unpack_constraints(c) for c in constraints]
-                for i, (id, hypos) in enumerate(zip(batch.ids.tolist(), translations)):
-                    src_tokens_i = utils.strip_pad(src_tokens[i], tgt_dict.pad())
-                    constraints = list_constraints[i]
-                    results.append((start_id + id, src_tokens_i, hypos,
-                                    { "constraints": constraints,
-                                      "time": translate_time / len(translations) }
-                                ))
-
-            # sort output to match input order
-            for id_, src_tokens, hypos, info in sorted(results, key=lambda x: x[0]):
-                if src_dict is not None:
-                    src_str = src_dict.string(src_tokens, args.remove_bpe)
-                    f_out.write('S-{}\t{}\n'.format(id_, src_str))
-                    f_out.write("W-{}\t{:.3f}\tseconds\n".format(id_, info["time"]))
-                    for constraint in info["constraints"]:
-                        f_out.write("C-{}\t{}\n".format(id_, tgt_dict.string(constraint, args.remove_bpe)))
-
-                # Process top predictions
-                for hypo in hypos[:min(len(hypos), args.nbest)]:
-                    hypo_tokens, hypo_str, alignment = utils.post_process_prediction(
-                        hypo_tokens=hypo['tokens'].int().cpu(),
-                        src_str=src_str,
-                        alignment=hypo['alignment'],
-                        align_dict=align_dict,
-                        tgt_dict=tgt_dict,
-                        remove_bpe=args.remove_bpe,
-                        extra_symbols_to_ignore=get_symbols_to_strip_from_output(generator),
-                    )
-                    detok_hypo_str = decode_fn(hypo_str)
-                    score = hypo['score'] / math.log(2)  # convert to base 2
-                    # original hypothesis (after tokenization and BPE)
-                    f_out.write('H-{}\t{}\t{}\n'.format(id_, score, hypo_str))
-
-                    exec_string = chared_to_string(hypo_str.split())
-                    f_out.write("Execution string:" + exec_string + "\n")
-                    try:
-                        result = test_prover.apply_theorem_seq_style(exec_string)
-                        # f_out.write(result)
-                    except Exception:
-                        continue
-                    if test_prover.is_proved():
-                        proofs_closed += 1
-                        break
-                    f_out.write("Objec: " + " ".join(filter_arrow(
-                        test_prover.parser.observation_to_source(test_prover.get_observation()))) + "\n")
-
-                    if result["progress"]:
-                        break
-
-                    # detokenized hypothesis
-                    f_out.write('D-{}\t{}\t{}\n'.format(id_, score, detok_hypo_str))
-                    f_out.write('P-{}\t{}\n'.format(
-                        id_,
-                        ' '.join(map(
-                            lambda x: '{:.4f}'.format(x),
-                            # convert from base e to base 2
-                            hypo['positional_scores'].div_(math.log(2)).tolist(),
-                        ))
-                    ))
-                    if args.print_alignment:
-                        alignment_str = " ".join(["{}-{}".format(src, tgt) for src, tgt in alignment])
-                        f_out.write('A-{}\t{}\n'.format(
-                            id_,
-                            alignment_str
-                        ))
-
-            # update running id_ counter
-            start_id += len(inputs)
-        if test_prover.is_proved():
-            f_out.write("Proof closed\n")
-        if not test_prover.is_proved():
-            f_out.write("Proof failed\n")
 
     logger.info("Total time: {:.3f} seconds; translation time: {:.3f}\n".format(time.time() - start_time, total_translate_time))
     f_out.write("We closed {} proofs out of {} in total. Proportion: {}\n"
