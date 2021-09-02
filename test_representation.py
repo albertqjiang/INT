aea64c2f9aab6345ae50b1c7a3ec1d7d5eb63b18
tomaszodrzygozdz <tomeko314@gmail.com>
2021-09-02 10:42:55 +0200

    Add action decoding

--- test_representation.py
+++ test_representation.py
@@ -1,6 +1,8 @@
 import json
 
+from algos.thm_env import TheoremProver
 from data_generation.generate_problems import generate_multiple_problems
+from representation.action_representation_decoder import pointer_str_to_action
 from representation.action_representation_pointer import ActionRepresentationPointer
 
 with open('data/benchmark/field/combinations.json', 'r') as file:
@@ -14,5 +16,9 @@ sample_entities = x[0]['all'].io_tuples[0][2]
 sample_action = (sample_axiom, *sample_entities)
 print(sample_action)
 
-action_rep = ActionRepresentationPointer.action_to_formula(sample_objective, sample_action)
-print(action_rep)
+action_rep_str = ActionRepresentationPointer.action_to_formula(sample_objective, sample_action)
+print(action_rep_str)
+
+decoded_action = pointer_str_to_action(sample_objective, action_rep_str)
+print(decoded_action)
+
