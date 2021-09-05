2e5ba367faa2becc0ce4fc30b334f2d83553c3bc
Albert Jiang <albert594250@gmail.com>
2021-09-05 20:38:45 +0100

    added method to convert names to logic statements and entities

--- visualization/seq_parse.py
+++ visualization/seq_parse.py
@@ -1,5 +1,7 @@
 import re
 from visualization.latex_parse import extract_two_operands
+from logic.utils import standard_numerical_functions, standard_logic_functions
+from logic.logic import Entity
 
 
 def rm_function_and_brackets(whole_string, function_name):
@@ -83,3 +85,71 @@ def logic_statement_name_to_seq_string(logic_statement_name):
 
 def logic_statement_to_seq_string(logic_statement):
     return logic_statement_name_to_seq_string(logic_statement.name)
+
+
+def name_to_entity(entity_name):
+    entity_name = re.sub(" ", "", entity_name)
+
+    all_numerical_function_names = [
+        "add", "sub", "mul", "opp", "sqr", "sqrt", "inv"
+    ]
+
+    if len(entity_name) == 1:
+        return Entity(entity_name)
+
+    outermost_name = None
+    found_name = False
+    for outermost_name in all_numerical_function_names:
+        if entity_name.startswith(outermost_name):
+            found_name = True
+            break
+    assert found_name
+
+    entity_name = rm_function_and_brackets(entity_name, outermost_name)
+    if standard_numerical_functions[outermost_name].input_no == 2:
+        two_operands = extract_two_operands(entity_name)
+        return standard_numerical_functions[outermost_name].execute_nf(
+            [name_to_entity(operand) for operand in two_operands]
+        )
+    else:
+        return standard_numerical_functions[outermost_name].execute_nf(
+            [name_to_entity(entity_name)]
+        )
+
+
+def name_to_ls(ls_name):
+    ls_name = re.sub(" ", "", ls_name)
+
+    all_logic_function_names = ["BiggerOrEqual", "SmallerOrEqual", "Equivalent"]
+    outermost_name = None
+    found_name = False
+    for outermost_name in all_logic_function_names:
+        if ls_name.startswith(outermost_name):
+            found_name = True
+            break
+    assert found_name
+
+    ls_name = rm_function_and_brackets(ls_name, outermost_name)
+    two_operands = extract_two_operands(ls_name)
+    return standard_logic_functions[outermost_name].execute_lf(
+        [name_to_entity(operand) for operand in two_operands]
+    )
+
+
+if __name__ == "__main__":
+    ls = standard_logic_functions["Equivalent"].execute_lf(
+        [
+            standard_numerical_functions["add"].execute_nf(
+                [
+                    Entity("b"),
+                    standard_numerical_functions["mul"].execute_nf(
+                        [Entity("c"), Entity("d")]
+                    )
+                ]
+            ),
+            standard_numerical_functions["opp"].execute_nf([Entity("a")])
+        ]
+    )
+
+    print(ls.name)
+    print(name_to_ls(ls.name).name)
