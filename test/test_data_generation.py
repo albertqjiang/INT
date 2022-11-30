49b0dfd7e254ea28169cc2d17af21eadd589a2e5
fzyzcjy <ch271828n@outlook.com>
2022-12-01 07:58:56 +0800

    generate_multiple_problems

--- test/test_data_generation.py
+++ test/test_data_generation.py
@@ -1,8 +1,9 @@
 from int_environment.data_generation.combos_and_orders import generate_combinations_and_orders
+from int_environment.data_generation.generate_problems import generate_multiple_problems
 from int_environment.proof_system.all_axioms import axiom_sets
 
 
-def test_combos_and_orders():
+def test_data_generation():
     axiom_combinations, axiom_orders = generate_combinations_and_orders(
         axiom_sets['field'],
         max_k=2,
@@ -26,3 +27,16 @@ def test_combos_and_orders():
 
     assert axiom_combinations == expect_axiom_combinations
     assert axiom_orders == expect_axiom_orders
+
+    datasets, problems = generate_multiple_problems(
+        num_axioms=2,
+        length=2,
+        num_probs=2,
+        train_test="train",
+        orders=axiom_orders,
+        degree=0,
+    )
+
+    # TODO: stronger checks
+    assert datasets is not None
+    assert problems is not None
