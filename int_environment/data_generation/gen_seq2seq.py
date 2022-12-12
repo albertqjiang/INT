ee34b783b7254551dcd72fcdd7367c9d40634ee7
fzyzcjy <ch271828n@outlook.com>
2022-12-12 16:15:08 +0800

    extract main

--- int_environment/data_generation/gen_seq2seq.py
+++ int_environment/data_generation/gen_seq2seq.py
@@ -1,12 +1,11 @@
 import argparse
 import json
 import os
-import random
 import pickle
+import random
 
 from int_environment.data_generation.generate_problems import generate_multiple_problems
 from int_environment.proof_system.graph_seq_conversion import Parser
-
 
 random.seed(213)
 proof_parser = Parser()
@@ -34,7 +33,7 @@ def generate_multiple_seq2seq(multiple_problems, all_sources_to_targets=None):
     return all_sources_to_targets
 
 
-if __name__ == "__main__":
+def main(args=None):
     parser = argparse.ArgumentParser(description='Mode generator')
     parser.add_argument('--orders_path',
                         default="/scratch/hdd001/home/ajiang/data/INT/ordered_field")
@@ -45,7 +44,7 @@ if __name__ == "__main__":
     parser.add_argument('--num_probs', type=int, default=1)
     parser.add_argument('--use_combos', action='store_true')
     parser.add_argument('--num_order_or_combo', type=int, default=None)
-    args = parser.parse_args()
+    args = parser.parse_args(args)
 
     if not os.path.isdir(args.dump_path):
         os.makedirs(args.dump_path)
@@ -54,7 +53,7 @@ if __name__ == "__main__":
     orders = json.load(open(os.path.join(args.orders_path, "orders.json"), "r"))
     if args.num_probs > 10000:
         sources_to_targets = None
-        for j in range(int(args.num_probs/1000)):
+        for j in range(int(args.num_probs / 1000)):
             if args.use_combos:
                 datasets, problems = generate_multiple_problems(num_axioms=args.k, length=args.l,
                                                                 num_probs=1000, train_test="train",
@@ -67,7 +66,7 @@ if __name__ == "__main__":
                                                                 num_order_or_combo=args.num_order_or_combo)
             sources_to_targets = generate_multiple_seq2seq(multiple_problems=problems,
                                                            all_sources_to_targets=sources_to_targets)
-            pickle.dump(problems, open(os.path.join(args.dump_path, "problems_part{}.pkl".format(j+1)), "wb"))
+            pickle.dump(problems, open(os.path.join(args.dump_path, "problems_part{}.pkl".format(j + 1)), "wb"))
     else:
         datasets, problems = generate_multiple_problems(num_axioms=args.k, length=args.l,
                                                         num_probs=args.num_probs, train_test="train",
@@ -87,3 +86,7 @@ if __name__ == "__main__":
                 src_out.write("\n")
                 tgt_out.write(sources_to_targets[key])
                 tgt_out.write("\n")
+
+
+if __name__ == "__main__":
+    main()
